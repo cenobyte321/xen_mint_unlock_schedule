590e5782c5fd010a0a9c7edb3068c34dcc5fa2a1
cenobyte321 <cenobyte321@clubdoom.com>
2023-01-10 07:34:41 -0600

    Refactoring and validation

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const fs = require('fs');
 const ics = require('ics');
 require('dotenv').config();
 
-// Setup the providers
-const ethereumProvider = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_RPC_URL);
-const avalancheProvider = new ethers.providers.JsonRpcProvider(process.env.AVALANCHE_RPC_URL);
-const fantomProvider = new ethers.providers.JsonRpcProvider(process.env.FANTOM_RPC_URL);
-
 // XEN contract address
 const XEN_ADDRESS_ETHEREUM  = "0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8";
 const XEN_ADDRESS_AVALANCHE = "0xC0C5AA69Dbe4d6DDdfBc89c0957686ec60F24389";
@@ -16,19 +11,7 @@ const XEN_ADDRESS_FANTOM    = "0xeF4B763385838FfFc708000f884026B8c0434275";
 const contractAbi = [
     "function balanceOf(address owner) view returns (uint256)",
     "function userMints(address user) view returns (address, uint256, uint256, uint256, uint256, uint256)",
-]
-
-const xenEthereumContract = new ethers.Contract(XEN_ADDRESS_ETHEREUM, contractAbi, ethereumProvider);
-xenEthereumContract.blockchain = "Ethereum";
-xenEthereumContract.urlPath = "mainnet"
-
-const xenAvalancheContract = new ethers.Contract(XEN_ADDRESS_AVALANCHE, contractAbi, avalancheProvider);
-xenAvalancheContract.blockchain = "Avalanche";
-xenAvalancheContract.urlPath = "avalanche";
-
-const xenFantomContract = new ethers.Contract(XEN_ADDRESS_FANTOM, contractAbi, fantomProvider);
-xenFantomContract.blockchain = "Fantom";
-xenFantomContract.urlPath = "fantom";
+];
 
 const generateEvent = (mintData, contract) => {
     [account, term, maturityTs, rank, amplifier, eaaRate] = mintData;
@@ -53,8 +36,34 @@ const generateEvent = (mintData, contract) => {
     }
 };
 
+const setupContracts = () =>{
+    const xenContracts = [];
+    // Setup the providers
+    const ethereumProvider = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_RPC_URL);
+    const avalancheProvider = new ethers.providers.JsonRpcProvider(process.env.AVALANCHE_RPC_URL);
+    const fantomProvider = new ethers.providers.JsonRpcProvider(process.env.FANTOM_RPC_URL);
+
+    const xenEthereumContract = new ethers.Contract(XEN_ADDRESS_ETHEREUM, contractAbi, ethereumProvider);
+    xenEthereumContract.blockchain = "Ethereum";
+    xenEthereumContract.urlPath = "mainnet";
+    xenContracts.push(xenEthereumContract);
+
+    const xenAvalancheContract = new ethers.Contract(XEN_ADDRESS_AVALANCHE, contractAbi, avalancheProvider);
+    xenAvalancheContract.blockchain = "Avalanche";
+    xenAvalancheContract.urlPath = "avalanche";
+    xenContracts.push(xenAvalancheContract);
+
+    const xenFantomContract = new ethers.Contract(XEN_ADDRESS_FANTOM, contractAbi, fantomProvider);
+    xenFantomContract.blockchain = "Fantom";
+    xenFantomContract.urlPath = "fantom";
+    xenContracts.push(xenFantomContract);
+    return xenContracts;
+}
+
 const main = async () => {
     console.log("Starting XEN Mint ICS generator...");
+    const xenContracts = setupContracts();
+
     console.log("Validate existence of accounts.txt file...");
     if (!fs.existsSync('accounts.txt')) {
         console.log("accounts.txt file not found. Exiting...");
@@ -63,8 +72,18 @@ const main = async () => {
     console.log("accounts.txt file found. Reading accounts...");
     const accounts = fs.readFileSync('accounts.txt', 'utf-8').split(/\r?\n/);
 
+    console.log("Verifying RPC connection...");
+    for (const contract of xenContracts) {
+        try {
+            await contract.provider.getBlockNumber();
+        } catch (e) {
+            console.error(`Error connecting to ${contract.blockchain} RPC. Please verify provided URL. Exiting...`, e);
+            return;
+        }
+    }
+
     const events = [];
-    for (const contract of [xenEthereumContract, xenAvalancheContract, xenFantomContract]) {
+    for (const contract of xenContracts) {
         console.log(`Getting mint data for ${contract.blockchain}...`);
         const promises = accounts.map(async (account) => {
              const mintData = await contract.userMints(account);
@@ -86,5 +105,5 @@ const main = async () => {
 };
 
 main().then(() => {
-    console.log("Finished processing all accounts.");
+    console.log("Finished!");
 });
